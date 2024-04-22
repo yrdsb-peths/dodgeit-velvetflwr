17ea019fd16cecd2a92eb8068a8d8594d10e8ed7
michelle <341078855@gapps.yrdsb.ca>
2024-04-22 17:53:54 -0400

    fast banana

--- banana.java
+++ banana.java
@@ -5,5 +5,23 @@ public class banana extends Actor
     public void act()
     {
         move(-3);
+        
+        if(getX()<=0)
+        {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int x = Greenfoot.getRandomNumber(2);
+        if(x==0)
+        {
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
     }
 }
