a96d09b966b64197406c97aab8ae1aa9915e375b
michelle <341078855@gapps.yrdsb.ca>
2024-04-22 17:51:19 -0400

    add banana

--- MyWorld.java
+++ MyWorld.java
@@ -10,5 +10,8 @@ public class MyWorld extends World
         Hero hero = new Hero();
         addObject(hero, 100, 100);
         
+        banana banana = new banana();
+        addObject(banana, 600, 100);
+        
     }
 }
